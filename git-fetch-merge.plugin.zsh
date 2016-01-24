eee2aa4e76807f32783feac18a99ee774b9105f0
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-01-24 11:27:11 -0200

    && :bomb:

--- git-fetch-merge.plugin.zsh
+++ git-fetch-merge.plugin.zsh
@@ -3,6 +3,5 @@ git-fetch-merge() {
   local remote=$(git remote | grep upstream || echo "origin")
   local branch="$1"
   test -z "$branch" && branch="master"
-  git fetch "$remote"
-  git merge "$remote/$branch"
+  git fetch "$remote" && git merge "$remote/$branch"
 }
